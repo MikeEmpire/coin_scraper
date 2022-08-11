2fb461f2a404fdd071949defd42035a2e1153375
Michael Olie <mike@grolens.com>
2022-08-11 08:40:20 -0700

    able to get data from ngc

--- get_ngc.py
+++ get_ngc.py
@@ -1,70 +1,170 @@
-
-from bs4 import BeautifulSoup
+import re
 import json
-import undetected_chromedriver as uc
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+chrome_options = Options()
+chrome_options.add_argument("--enable-javascript")
 
-f = open('coin_category_links.json')
-coin_category_links = json.load(f)
+category_file_name = "coin_category_links.json"
+coin_page_file_name = "coin_page_links.json"
 
-file = open('coin_type_links.json')
-coin_type_links = json.load(file)
-
-uc_opts = uc.ChromeOptions()
-driver = uc.Chrome(use_subprocess=True, options=uc_opts)
-base_url = 'https://www.ngccoin.com/coin-explorer'
-# CHOOSE_A_COIN = "Choose a Coin Type:"
-# initial_url = 'https://www.ngccoin.com/coin-explorer/coinfacts'
-# driver.get(initial_url)
-driver.get(base_url)
-html = driver.page_source
-soup = BeautifulSoup(html, "html.parser")
-box_class = 'ccg-card-grid__item ng-scope'
-selected_class = 'ccg-card-grid__item ng-scope ccg-resource-card--selected'
-card_boxes = soup.find_all('div', {"class": box_class})
-for box in card_boxes:
-    box.click
-
-# coin_page_links = []
-# coin_category_page_links = []
+path = '/usr/local/bin/chromedriver'
 
 
 def get_element_links(element, array):
-    for link in element.find_all('a', href=True):
-        coin_page_link = link.get('href')
-        link_to_add = f'{base_url}{coin_page_link}'
-        array.append(link_to_add)
+    for anchor in element.find_elements(By.TAG_NAME, 'a'):
+        try:
+            link = anchor.get_attribute('href')
+            array.append(link)
+        except:
+            pass
 
 
-# for box in category_boxes:
-#     for list_element in box.find_all('li'):
-#         get_element_links(list_element, category_page_links)
+driver = webdriver.Chrome(service=Service(path), options=chrome_options)
 
 
-# go through each page link,
-# for page_link in coin_category_links:
-#     driver.get(page_link)
-#     cat_html = driver.page_source
-#     cat_page_soup = BeautifulSoup(cat_html, 'html.parser')
-#     tall_text_class = 'container-fluid padding-bottom text-tall-lh'
-#     is_pre_page = cat_page_soup.find('div', {"class": tall_text_class})
-#     new_links = new_links.remove(page_link)
-#     if is_pre_page is None:
-#         row_class = 'row cf-cat-list padding-top text-center vertical-top'
-#         coin_type_div = cat_page_soup.find('div', {"class": row_class})
-#         get_element_links(coin_type_div, coin_type_links)
+def open_file(filename):
+    file_object = open(filename, "r")
+    json_content = file_object.read()
+    return json.loads(json_content)
 
 
-# check if page has text: "Select a coin" OR "choose a type"
-# 	loop through tabs, get all coin links
+def write_new_file(array, filename):
+    file_to_erase = open(filename, 'r+')
+    file_to_erase.truncate(0)
+    data_json = json.dumps(array)
+    new_file = open(filename, "w")
+    new_file.write(data_json)
+    new_file.close()
 
 
-driver.close()
+def close_modal(driver):
+    try:
+        modal = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'email-signup')))
 
-# file_to_erase = open('coin_page_links.json', 'r+')
-# file_to_erase.truncate(0)
-# page_json = json.dumps(new_links)
-# new_file = open("coin_page_links.json", "w")
-# new_file.write(page_json)
-# new_file.close()
+        close_icon = modal.find_element(By.CLASS_NAME, "modal-dialog-close")
+        close_icon.click()
+    except:
+        print('unable to find close icon')
+        pass
 
+
+def get_coin_category_links():
+    base_url = 'https://www.ngccoin.com/coin-explorer'
+
+    driver.get(base_url)
+
+    close_modal(driver)
+
+    outer_category_class = 'ccg-card-grid__item'
+
+    coin_grid_container = driver.find_element(
+        By.CLASS_NAME, 'ccg-card-grid__inner')
+
+    outer_categories = coin_grid_container.find_elements(
+        By.CLASS_NAME, outer_category_class)
+
+    category_page_links = []
+    cat_index = 1
+    for outer_category in outer_categories:
+        outer_category.click()
+        get_element_links(outer_category, category_page_links)
+        close_xpath = f'/html/body/div[2]/div[2]/div/div[1]/div/div/div[3]/div[2]/div[{cat_index}]/div/div[2]/div/div/div[2]/div[1]'
+
+        try:
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, close_xpath)))
+            close_icon = outer_category.find_element(
+                By.XPATH, close_xpath).click()
+            driver.implicitly_wait(5)
+            cat_index += 1
+        except:
+            break
+
+    driver.close()
+
+    write_new_file(category_page_links, category_file_name)
+
+
+def get_coin_page_links():
+    # load category links
+    coin_page_links = []
+    coin_categories = open_file(category_file_name)
+    for category_link in coin_categories:
+        # go to page
+        driver.get(category_link)
+        # find div.ce-tab-contents
+        try:
+            close_modal(driver)
+            tab = driver.find_element(By.CLASS_NAME, 'ce-tab-contents')
+            # get all element links
+            get_element_links(tab, coin_page_links)
+            driver.implicitly_wait(5)
+        except:
+            pass
+    driver.close()
+    write_new_file(coin_page_links, coin_page_file_name)
+
+
+def scrape_coin_data():
+    coin_data = []
+    coin_page_links = open_file(coin_page_file_name)
+    for coin_page_link in coin_page_links:
+        driver.get(coin_page_link)
+        coin_data_object = {}
+        barcode = coin_page_link.split('/')[7]
+        coin_data_object['barcode'] = barcode
+
+        def get_coin_info(soup):
+            try:
+                data_list = soup.find('ul', {'class': 'ce-coin__specs-list'})
+                list_elements = data_list.find_all('li')
+                for list_element in list_elements:
+                    raw_list_string = list_element.text
+                    data = re.sub(r'\n', "", raw_list_string).split(":")
+                    key = data[0]
+                    value = data[1]
+                    coin_data_object[key] = value
+            except:
+                pass
+        try:
+            close_modal(driver)
+            html = driver.page_source
+
+            soup = BeautifulSoup(html, "html.parser")
+
+            raw_coin_description = soup.select_one(
+                'body > div.ccg-canvas > div.ccg-body > div.inner-main > div > div > div.ce-coin__topbar.ng-scope > div > div:nth-child(3) > div.ce-coin__title > h1').text
+
+            # GET PRODUCT DESCRIPTION
+            coin_data_object['Description'] = raw_coin_description
+            get_coin_info(soup)
+            grade_scroller_table = soup.find('div', {"id": 'gradeSCroller'})
+            data_rows = grade_scroller_table.find_all('tr')
+            grades = []
+            grade_row = data_rows[0]
+            for grade in grade_row.find_all('td'):
+                grades.append(grade.text)
+            price_row = data_rows[1]
+            for index, price in price_row.find_all('td'):
+                coin_data_object['price'][grades[index]] = price.text
+            pop_row = data_rows[2]
+            for index, pop in pop_row.find_all('td'):
+                coin_data_object['pop'][grades[index]] = pop.text
+            # loop through table row
+            # set the index of the header as the key
+            coin_data.append(coin_data_object)
+        except:
+            pass
+    driver.close()
+    write_new_file(coin_data, "coin_data.json")
+
+
+scrape_coin_data()
